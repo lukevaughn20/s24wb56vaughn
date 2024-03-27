3163aaf8794dee875f0e5ff64d6f7e7b02cdd151
lukevaughn20 <78869404+lukevaughn20@users.noreply.github.com>
2024-03-27 16:56:39 -0500

    part 5 try2

--- routes/pizza.js
+++ routes/pizza.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pizza', { title: 'Search Result Pizza' });
 });
 
 module.exports = router;
